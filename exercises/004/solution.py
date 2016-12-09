e3cf790b0f11e8c832c974852203476f41260d1f
Claire Baudelet <claire.baudelet@cri-paris.org>
2016-12-09 16:49:35 +0100

    exo04

---
+++ exercises/004/solution.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+n = string.ascii_lowercase[::-1]
+j = n.replace('a', 'A')
+m = j.replace('e', 'E')
+n = m.replace('i', 'I')
+o = n.replace('o', 'O')
+p = o.replace('u', 'U')
+print(p)
